41ed9f5f7a6a65641ac9011522fa0fa750f41114
Supul Pushpakumara <supul.19@cse.mrt.ac.lk>
2022-06-27 14:19:04 +0530

    navbar completed

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -19,14 +19,11 @@ function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link href="#features">About</Nav.Link>
-                    <Nav.Link href="#pricing">Logout</Nav.Link>
+                    <Nav.Link href="#">About</Nav.Link>
+                    <Nav.Link href="#">Logout</Nav.Link>
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#deets">Contact Us</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-                    </Nav.Link>
+                    <Nav.Link href="#">Contact Us</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
