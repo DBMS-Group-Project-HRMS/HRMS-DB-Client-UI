bc24237bfce9ee706c769d192e110ce520ea9c4b
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2022-07-01 22:47:58 +0530

    Employee by department report creation done

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -15,6 +15,7 @@ import { CreateLeavesInPeriodByDepartmentReport } from "../views/Report/CreateLe
 import { CreateEmployeeDetailsReport } from "../views/Report/CreateEmployeeDetailsReport";
 import { CreateCustomReport1 } from "../views/Report/CreateCustomReport1";
 import { CreateCustomReport2 } from "../views/Report/CreateCustomReport2";
+import { EmployeeByDepartmentReport } from "../views/Report/EmployeeByDepartmentReport";
 import SupervisorHome from "../views/Supervisor/SupervisorHome";
 import HRHome from "../views/HRManager/HRHome";
 import ManagerHome from "../views/manager/ManagerHome";
@@ -40,6 +41,7 @@ export function Router(){
         <Route path="/reports/createEmployeeDetailsReport" element={<LevelAuthRoute levels={['level 3']}> <CreateEmployeeDetailsReport /> </LevelAuthRoute>}/>
         <Route path="/reports/createCustomReport1" element={<LevelAuthRoute levels={['level 3']}> <CreateCustomReport1 /> </LevelAuthRoute>}/>
         <Route path="/reports/createCustomReport2" element={<LevelAuthRoute levels={['level 3']}> <CreateCustomReport2 /> </LevelAuthRoute>}/>
+        <Route path="/reports/employeeByDepartmentReport" element={<LevelAuthRoute levels={['level 3']}> <EmployeeByDepartmentReport /> </LevelAuthRoute>}/>
         <Route path="/employeeHome" element={<LevelAuthRoute levels={['level 1']}> <EmployeeHome /> </LevelAuthRoute>} />
         <Route path="/manager/view_user/:user_id"element={<LevelAuthRoute levels={['level 4', 'level 3']}> <ViewUser /> </LevelAuthRoute>}/>
         <Route path="/manager/view_users_list" element={<LevelAuthRoute levels={['level 4', 'level 3']}> <ViewUsersList /> </LevelAuthRoute>} />
--- client/src/views/Report/CreateEmployeeByDepartmentReport.js
+++ client/src/views/Report/CreateEmployeeByDepartmentReport.js
@@ -4,19 +4,59 @@ import { useNavigate, Link } from "react-router-dom";
 import Navbar from "../../navbar/navbar";
 
 export function CreateEmployeeByDepartmentReport() {
-  const parameterList = ["a", "b", "c", "d", "e"]
-
   const [departmentList, setDepartmentList] = useState([]);
-  const initialValues = { department: "", parameters: [] };
-  const [formValues, setformValues] = useState(initialValues);
+  const [parameterList, setParameterList] = useState([]);
+  const [department, setDepartment] = useState("");
+  const [parameters, setParameters] = useState(null);
+  const [formValues, setformValues] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [show, setShow] = useState(false);
   const [alertType, setAlertType] = useState("");
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSubmit) {
+      console.log(formValues);
+      console.log("creating a report");
+      let token = sessionStorage.getItem("token");
+      Axios.post("http://localhost:3001/report/create_employee_by_department_report", formValues, { headers:{Authorization : `Bearer ${token}`} })
+        .then( (response)=>{
+          setAlertType("alert alert-success");
+          setAlertMessage(response.data.message);
+        })
+        .catch((err) => {
+          setAlertType("alert alert-danger");
+          setAlertMessage("");
+          switch (err.response.request.status) {
+            case 400:
+              setAlertMessage(err.response.data.message);
+              setShow(true);
+              break;
+            case 500:
+              setAlertMessage("Server Error!");
+              setShow(true);
+              break;
+            case 501:
+              setAlertMessage("Server Error!");
+              setShow(true);
+              break;
+            case 502:
+              setAlertMessage("Server Error!");
+              setShow(true);
+              break;
+            default:
+              break;
+          }
+        });
+      setIsSubmit(false);
+    }
+  },[isSubmit]);
+
   useEffect(() => {
     let token = sessionStorage.getItem("token");
-      Axios.get("http://localhost:3001/report/get_department_list", { headers:{Authorization : `Bearer ${token}`} })
+    Axios.get("http://localhost:3001/report/get_department_list", { headers:{Authorization : `Bearer ${token}`} })
     .then((resDepartmentList) => {
       setDepartmentList(resDepartmentList.data.data);
     })
@@ -44,31 +84,69 @@ export function CreateEmployeeByDepartmentReport() {
           break;
       }
     });
-  }, []);
+  },[]);
+
+  useEffect(() => {
+    let token = sessionStorage.getItem("token");
+    Axios.get("http://localhost:3001/report/get_employee_by_department_report_parameters", { headers:{Authorization : `Bearer ${token}`} })
+    .then((resParameterList) => {
+      setParameterList(resParameterList.data.data);
+    })
+    .catch((err) => {
+      setAlertMessage("");
+      setAlertType("alert alert-danger");
+      switch (err.response.request.status) {
+        case 400:          
+          setAlertMessage(err.response.data.message);
+          setShow (true);
+          break;
+        case 500:
+          setAlertMessage("Server Error!");
+          setShow (true);
+          break;
+        case 501:
+          setAlertMessage("Server Error!");
+          setShow (true);
+          break;
+        case 502:
+          setAlertMessage("Server Error!");
+          setShow (true);
+          break;
+        default:
+          break;
+      }
+    });
+  },[]);
+
+  useEffect(() => {
+    setParameters(new Array(parameterList.length).fill(false));
+  },[parameterList]);
+
+  useEffect(() => {
+    setformValues({ ...formValues, "department":department });
+  }, [department]);
+
+  useEffect(() => {
+    setformValues({ ...formValues, "parameters":parameters });
+  }, [parameters]);
 
   const handleDepartmentChange = (e) => {
     const { name, value } = e.target;
-    setformValues({ ...formValues, [name]:value });
+    setDepartment(value);
   };
 
-  const handleParameterChange = (e) => {
-    const { name, value } = e.target;
-    const index = initialValues.parameters.indexOf(value);
-    if (index > -1)
-      initialValues.parameters.splice(index, 1);
-    else
-      initialValues.parameters.push(value);
-
-    console.log(initialValues.parameters);
-
-    // setformValues({ ...formValues, [name]: initialValues.parameters });
+  const handleParameterChange = (position) => {
+    const updatedCheckedParameters = parameters.map((parameter, index) =>
+      index === position ? !parameter : parameter
+    );
     
+    setParameters(updatedCheckedParameters);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formValues);
     setIsSubmit(true);
+    navigate('/reports/employeeByDepartmentReport',{state:{formValues}});
   }
 
   return (
@@ -83,9 +161,8 @@ export function CreateEmployeeByDepartmentReport() {
 
           <div className="form-group mb-3">
             <label className="label" class="h3">Department</label>
-            <select className="custom-select custom-select-lg mb-3 ml-4 w-25" name="department" id="department" value={formValues.department} onChange={handleDepartmentChange} >
-              <option disabled defaultValue="">Select Department</option>
-              <option value="All">All</option>
+            <select className="custom-select custom-select-lg mb-3 ml-4 w-25" name="department" id="department" value={formValues.department} onChange={handleDepartmentChange} required>
+              <option disabled selected value="" >Select Department</option>
               {departmentList.map((department) => <option key={department.Name} value={department.Name}>{department.Name}</option>)}
             </select>
           </div>
@@ -95,18 +172,19 @@ export function CreateEmployeeByDepartmentReport() {
             <ul className="parameter-list w-25 pl-5 m-0">
               {parameterList.map((name, index) => {
                 return (
-                    <div className="parameter-list-item">
-                      <div className="left-section">
-                        <input
-                          type="checkbox"
-                          id={`custom-checkbox-${index}`}
-                          name="parameters"
-                          value={name}
-                          onChange={handleParameterChange}
-                        />
-                        <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                      </div>
+                  <div className="parameter-list-item">
+                    <div className="left-section">
+                      <input
+                        type="checkbox"
+                        id={`custom-checkbox-${index}`}
+                        name="parameters"
+                        value={name.COLUMN_NAME}
+                        checked={parameters[index]}
+                        onChange={() => handleParameterChange(index)}
+                      />
+                      <label htmlFor={`custom-checkbox-${index}`}>{name.COLUMN_NAME}</label>
                     </div>
+                  </div>
                 );
               })}
             </ul>
@@ -122,8 +200,8 @@ export function CreateEmployeeByDepartmentReport() {
 
       </div>
 
-    <Link to="/reports"><button className="btn btn-outline-primary my" >Back</button></Link>
-    <Link to="/"><button className="btn btn-outline-primary my" >Back Home</button></Link>
+      <Link to="/reports"><button className="btn btn-outline-primary my" >Back</button></Link>
+      <Link to="/"><button className="btn btn-outline-primary my" >Back Home</button></Link>
 
     </div>
   );
