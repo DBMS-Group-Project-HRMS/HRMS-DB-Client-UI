4bd2f7fe252e87214bb183477a21d0035a90e1a8
Supul Pushpakumara <supul.19@cse.mrt.ac.lk>
2022-07-04 01:39:17 +0530

    employee home updated with css

--- client/src/views/Employee/EmployeeHome.js
+++ client/src/views/Employee/EmployeeHome.js
@@ -35,10 +35,20 @@ class EmployeeHome extends Component {
 
     return (
       <React.Fragment>
-        <div className="Container-fluid shadow ">
-        <h1 class="text-center mt-3 mb-0">Employee Home</h1>
+        <div className="Container-fluid ">
+          <h1 class="text-center mt-3 mb-0">Employee Home</h1>
           <br></br>
             <div className='row'>
+              <div className="col-6">
+              <img
+                    alt=""
+                    src="assets/images/vec1.png"
+                    // width="100"
+                    // height="100"
+                    className="d-inline-block align-top"
+                    />{' '}
+              </div>
+              <div className="col-3">
                 <Link to={'/user/apply_leave'}>
                   <Button 
                   outline color="dark" 
@@ -47,15 +57,19 @@ class EmployeeHome extends Component {
                       Apply Leave
                   </Button>
                 </Link>
-
+              </div>
+              <div className="col-3"></div>
                 <br></br>
                 <br></br>
-                <Button outline color="dark" className="shadow-sm col-6">
-                    Edit Profile
-                </Button>
-
-                <br></br>
-                <br></br>
+                <div className="col-3">
+                <Link to={'/user/view_profile'}>
+                  <Button outline color="dark" className="shadow-sm col-6">
+                      Edit Profile
+                  </Button>
+                </Link>
+                  <br></br>
+                  <br></br>
+                </div>
             </div>
         </div>
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
